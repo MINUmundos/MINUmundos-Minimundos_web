ff1aecec134881c854b113d1d7e9d8193ffc582b
JPMINIMundos <wcjos@wcjos.wcjos>
2025-07-20 20:50:42 +0200

    first deploy

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  base: '/Minimundos_web/' // 👈 Required for GitHub Pages
-})
